badd52cb5a9185fcab5a55bcbe2e1bd13d31c710
Sergey Korotkih <44646306+yaspokoyen@users.noreply.github.com>
2023-01-17 12:27:34 +0300

    Update WelcomeController.java

--- src/main/java/example/smallest/controllers/WelcomeController.java
+++ src/main/java/example/smallest/controllers/WelcomeController.java
@@ -16,6 +16,6 @@ public class WelcomeController {
 		
 		
 		
-		return "Hello FUCKING World!!!"; //"application/json" mean this is a text not a redirect
+		return "Hello черт побери World!!!"; //"application/json" mean this is a text not a redirect
 	}
 }
